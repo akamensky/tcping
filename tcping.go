a2af1656a9c3b7b01a076f7c1154d436088c5900
Alexey Kamenskiy <alexey.kamenskiy@macbook.local>
2018-06-06 13:26:48 +0800

    Fix floating division and allow to set period

--- tcping.go
+++ tcping.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"github.com/akamensky/argparse"
-	"os"
 	"fmt"
-	"time"
+	"github.com/akamensky/argparse"
 	"net"
+	"os"
+	"time"
 )
 
 func main() {
@@ -13,7 +13,8 @@ func main() {
 
 	server := p.String("s", "server", &argparse.Options{Required: true, Help: "Remote server to check"})
 	port := p.Int("p", "port", &argparse.Options{Required: true, Help: "Remote port to check"})
-	timeoutArg := p.Int("t", "timeout", &argparse.Options{Default: 500, Help: "Connection timeout in ms"})
+	timeoutArg := p.Int("t", "timeout", &argparse.Options{Default: 500, Help: "Connection timeout in ms. Should be less than check period"})
+	periodArg := p.Int("", "period", &argparse.Options{Default: 1000, Help: "Period of check in ms"})
 
 	err := p.Parse(os.Args)
 	if err != nil {
@@ -21,14 +22,20 @@ func main() {
 		os.Exit(1)
 	}
 
-	ticker := time.NewTicker(1 * time.Second)
-	quit := make(chan interface{})
-
 	var seqNumber uint64 = 0
 	var network = fmt.Sprintf("%s:%d", *server, *port)
 	var timeout = time.Duration(*timeoutArg) * time.Millisecond
+	var period = time.Duration(*periodArg) * time.Millisecond
 
-	for ;;seqNumber++ {
+	if timeout >= period {
+		fmt.Print(p.Usage(fmt.Errorf("timeout should be less than period")))
+		os.Exit(1)
+	}
+
+	ticker := time.NewTicker(period)
+	quit := make(chan interface{})
+
+	for ; ; seqNumber++ {
 		select {
 		case <-ticker.C:
 			tryPort(network, seqNumber, timeout)
@@ -46,7 +53,8 @@ func tryPort(network string, seq uint64, timeout time.Duration) {
 	if err != nil {
 		os.Stdout.Write([]byte(startTime.Format("[2006-01-02T15:04:05]:") + " connection failed\n"))
 	} else {
-		var t = float64(endTime.Sub(startTime)/time.Millisecond)
+		defer conn.Close()
+		var t = float64(endTime.Sub(startTime)) / float64(time.Millisecond)
 		os.Stdout.Write([]byte(startTime.Format("[2006-01-02T15:04:05]:") + fmt.Sprintf(" addr=%s seq=%d time=%4.2fms\n", conn.RemoteAddr().String(), seq, t)))
 	}
 }
